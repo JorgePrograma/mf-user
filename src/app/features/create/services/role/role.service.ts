5c6758299f766fb31e11124bef2dd1b65856cab8
jorgePrograma <jemartelo07@gmail.com>
2025-04-14 16:12:59 -0500

    none

--- src/app/features/create/services/role/role.service.ts
+++ src/app/features/create/services/role/role.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, signal } from '@angular/core';
 import { catchError, map, Observable, of, tap } from 'rxjs';
 import { Config } from '../../core/utils/config';
@@ -10,7 +10,7 @@ import { Role } from '../../model/role.model';
   providedIn: 'root',
 })
 export class RoleService {
-   // Señal privada para el estado interno
+  // Señal privada para el estado interno
   private readonly _roles = signal<Role[]>([]);
 
   // Señal pública de solo lectura
@@ -19,29 +19,38 @@ export class RoleService {
   constructor(private readonly http: HttpClient) {}
 
   getAllRoles(): Observable<Role[]> {
-    return this.http.get<ResponseModel<string>>(EndPoints.GET_ALL_ROLES,{
-      headers:{
-        Authorization:'Bearer '+ Config.TOKEN
-      }
-    }).pipe(
-      map((response) => {
-        try {
-          const parsedData = JSON.parse(response.data) as { roles: Role[] };
-          console.log("roles", parsedData)
-          return parsedData.roles || [];
-        } catch (error) {
-          console.error('Error parseando JSON:', error);
-          return [];
-        }
-      }),
-      tap((roles) => {
-        this._roles.set(roles);
-        console.log('Roles actualizadas:', roles);
-      }),
-      catchError((error) => {
-        console.error('Error en la solicitud HTTP:', error);
-        return of([]);
-      })
-    );
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${Config.TOKEN}`, // Asegúrate de tener un método getToken()
+      // Si necesitas otros headers específicos para CORS, agrégalos aquí
+      'Access-Control-Allow-Origin': '*', // Normalmente esto se configura en el backend
+    });
+    const options = { headers };
+
+    return this.http
+      .get<ResponseModel<string>>(
+        'https://femrwzf6x6uakaqkb32tl27hgm.apigateway.sa-bogota-1.oci.customer-oci.com/api/v1/Roles/GetAllRole/',
+        options
+      )
+      .pipe(
+        map((response) => {
+          try {
+            const parsedData = JSON.parse(response.data) as { roles: Role[] };
+            console.log('roles', parsedData);
+            return parsedData.roles || [];
+          } catch (error) {
+            console.error('Error parseando JSON:', error);
+            return [];
+          }
+        }),
+        tap((roles) => {
+          this._roles.set(roles);
+          console.log('Roles actualizadas:', roles);
+        }),
+        catchError((error) => {
+          console.error('Error en la solicitud HTTP:', error);
+          return of([]);
+        })
+      );
   }
 }
